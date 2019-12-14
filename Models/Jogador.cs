f7e854dc26079c74becb11993c4fe19ea8b9f07f
Adauto <dadorpm@hotmail.com>
2019-12-14 00:46:58 -0300

    criacao das models

---
+++ Models/Jogador.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Jogo.Models
+{
+    public class Jogador
+    {
+        public int JogadorId { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int NacionalidadeId { get; set; }
+        public virtual Nacionalidade Nacionalidade { get; set; }
+
+    }
+}
---
+++ Models/Nacionalidade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jogo.Models
+{
+    public class Nacionalidade
+    {
+        public int NacionalidadeId { get; set; }
+        public string Name { get; set; }
+    }
+}
