08ba65109f1b1aad3ea4388784b27e894c42b5cb
Adauto <dadorpm@hotmail.com>
2019-12-15 20:19:39 -0300

    correções

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,34 +1,29 @@
 ﻿
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Jogo.Models;
+using Microsoft.EntityFrameworkCore;
+using Jogo.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace Jogo.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
+        }
+  
+
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Placar.Include(p => p.Jogador).OrderByDescending(p => p.Pontos).Take(10);
+            return View(await applicationDbContext.ToListAsync());
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    
     }
 }
